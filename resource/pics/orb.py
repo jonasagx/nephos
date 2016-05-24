14f11a7b08d68ab5c21776d9b7c0e9ebe07b9eac
jonasagx <jonas.agx@gmail.com>
2016-05-24 09:56:00 -0300

    Experiment results

--- resource/pics/orb.py
+++ resource/pics/orb.py
@@ -1,5 +1,5 @@
 import sys
-sys.path.append("/usr/local/lib/python2.7/site-packages/")
+# sys.path.append("/usr/local/lib/python2.7/site-packages/")
 import numpy as np
 import cv2
 
@@ -10,15 +10,16 @@ img2 = cv2.imread(sys.argv[2]) # Rotated image
 # of 1.2
 #orb = cv2.ORB_create(1000, 1.2)
 
-surf = cv.xfeatures2d.SURF_create()
+detector = cv2.xfeatures2d.SURF_create()
+# detector = cv2.ORB_create()
 # Detect keypoints of original image
-(kp1,des1) = surf.detectAndCompute(img1, None)
+(kp1,des1) = detector.detectAndCompute(img1, None)
 
 # Detect keypoints of rotated image
-(kp2,des2) = surf.detectAndCompute(img2, None)
+(kp2,des2) = detector.detectAndCompute(img2, None)
 
 # Create matcher
-bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
+bf = cv2.BFMatcher(crossCheck=True)
 
 # Do matching
 matches = bf.match(des1,des2)
@@ -28,9 +29,10 @@ matches = bf.match(des1,des2)
 #matches = sorted(matches, key=lambda val: val.distance)
 
 # Show only the top 10 matches
-img3 = cv2.drawMatches(img1, kp1, img2, kp2, matches[:10], None, flags=2)
+img3 = cv2.drawMatches(img1, kp1, img2, kp2, matches[:20], None, flags=2)
 
 cv2.imshow("Match", img3)
 if cv2.waitKey(0) & 0xff == 27:
     cv2.destroyAllWindows()
 
+cv2.imwrite("match.png", img3)
--- src/plotter.r
+++ src/plotter.r
@@ -1,13 +1,13 @@
-filename = "orb_vectors.csv"
+filename = "orb_vectors_sub.csv"
 # print(filename)
 
 d <- read.csv(file=filename, head=TRUE, sep=',')
 
 chart = "vector_map.png"
 
-png(chart, width=550, height=550)
+png(chart, width=500, height=500)
 
-plot(NA, xlim=c(0, 500), ylim=c(0, 500), main="Wind vectros' map", xlab="latitude", ylab="longitude")
+plot(NA, xlim=c(0, 200), ylim=c(0, 200), main="Wind vectros' map", xlab="latitude", ylab="longitude")
 arrows(d$x1, d$y1, d$x2, d$y2, length=0.09)
 
 dev.off()
