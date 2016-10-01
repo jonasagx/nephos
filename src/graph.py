78dd412b22b010f944d1dfe43edc2e989b25cda1
Jonas Xavier <jonas.agx@gmail.com>
2016-10-01 19:01:09 -0300

    All

--- src/graph.py
+++ src/graph.py
@@ -1,30 +1,32 @@
 import sys
-import cv2 as cv
-from util import printer
+# import cv2 as cv
+# from util import printer
 from util import loadFiles
 from util import validSerie
-from util import seekMatches
-from util import matchPrinter
-from util import basicFormater
-from util import globalPrinter
-from util import countPositives
-from util import countMatchesByPair
-from util import countKeypointsByPhoto
-from util import processingTimeByPhoto
-from util import flatView
+# from util import seekMatches
+# from util import matchPrinter
+# from util import basicFormater
+# from util import globalPrinter
+# from util import countPositives
+# from util import countMatchesByPair
+# from util import countKeypointsByPhoto
+# from util import processingTimeByPhoto
+# from util import flatView
 
 path = sys.argv[1]
 rawFilesList = loadFiles(path)
 filesList = validSerie(rawFilesList)
 
-matcher = cv.BFMatcher(crossCheck=True)
+print filesList
+
+#matcher = cv.BFMatcher(crossCheck=True)
 # detector = cv.xfeatures2d.SURF_create()
 # detector = cv.xfeatures2d.SIFT_create()
-detector = cv.ORB_create()
+# detector = cv.ORB_create()
 
-matches_set = seekMatches(detector, matcher, filesList, path)
-filtered = basicFormater(matches_set)
-flat = flatView(filtered)
+#matches_set = seekMatches(detector, matcher, filesList, path)
+#filtered = basicFormater(matches_set)
+#flat = flatView(filtered)
 # print printer(flat)
-print globalPrinter(matches_set, filesList, path)
+#print globalPrinter(matches_set, filesList, path)
 # print matchPrinter(filtered)
