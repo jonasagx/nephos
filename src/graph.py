9d07fe0c85e1248d5c32a178bb2b1e9327b9c157
Jonas Xavier <jonas.agx@gmail.com>
2017-02-17 15:52:55 -0800

    Global experiemnt

--- src/graph.py
+++ src/graph.py
@@ -1,7 +1,9 @@
 import sys
+import csv
 import cv2 as cv
 from util import printer
 from util import loadFiles
+from util import saveToFile
 from util import validSerie
 from util import seekMatches
 from util import matchPrinter
@@ -12,18 +14,33 @@ from util import countMatchesByPair
 from util import countKeypointsByPhoto
 from util import processingTimeByPhoto
 
-path = sys.argv[1]
-rawFilesList = loadFiles(path)
-filesList = validSerie(rawFilesList)
+def main():
+	path = sys.argv[1]
+	rawFilesList = loadFiles(path)
+	filesList = validSerie(rawFilesList)
 
-matcher = cv.BFMatcher(crossCheck=True)
-# detector = cv.xfeatures2d.SIFT_create()
-detector = cv.ORB_create()
+	matcher = cv.BFMatcher(crossCheck=True)
 
-matches_set = seekMatches(detector, matcher, filesList, path)
-filtered = basicFormater(matches_set)
-print(printer(filtered))
-# print globalPrinter(matches_set, filesList, path)
-# print matchPrinter(filtered)
+	siftDetector = cv.xfeatures2d.SIFT_create()
+	surfDetector = cv.xfeatures2d.SURF_create()
+	orbDetector = cv.ORB_create()
+
+	runExperiemnt(siftDetector, matcher, filesList, path, "sift")
+	runExperiemnt(surfDetector, matcher, filesList, path, "surf")
+	runExperiemnt(orbDetector, matcher, filesList, path, "orb")
 
 
+def runExperiemnt(detector, matcher, filesList, path, algorithmName):
+	matches_set = seekMatches(detector, matcher, filesList, path)
+	filtered = basicFormater(matches_set)
+
+	dataFiltered = printer(filtered)
+	saveToFile(algorithmName + "-filtered.csv", dataFiltered)
+
+	dataGlobal = globalPrinter(matches_set, filesList, path)
+	saveToFile(algorithmName + "-global.csv", dataGlobal)
+
+	dataMatch = matchPrinter(filtered)
+	saveToFile(algorithmName + "-match.csv", dataMatch)
+
+main()
