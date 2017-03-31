089a091d0614ecb62bb45ad5a16fa6798cce1cbe
Jonas Xavier <jonas.agx@gmail.com>
2017-03-30 22:36:08 -0700

    Configs for experiments I

--- src/findingTest.py
+++ src/findingTest.py
@@ -11,6 +11,8 @@ import pprint
 
 import cv2 as cv
 import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
 
 from pymongo import MongoClient
 
@@ -18,6 +20,16 @@ class Algorithm:
 	def __init__(self, name, detector):
 		self.name = name
 		self.detector = detector
+
+class NegriDetector:
+	def __init__(self, minValue, maxValue):
+		self.minValue = minValue
+		self.maxValue = maxValue
+
+	def detectAndCompute(self, im, something=None):
+		imFlat = im.flatten()
+		size = ((imFlat >= self.minValue) & (imFlat <= self.maxValue)).sum()
+		return (np.zeros(size), None)
 
 def getCollection(host, port, dbName, collectionName):
 	client = MongoClient(host, port)
@@ -36,7 +48,8 @@ def runSerie(cvImages, detector):
 
 	for index, image in enumerate(cvImages):
 		(kp1, des1) = detector.detectAndCompute(image, None)
-		serie.append(len(kp1))
+		l = len(kp1)
+		serie.append(np.log10(l))
 	return serie
 
 def convertToImages(imagesFromDb):
@@ -52,21 +65,33 @@ def convertToImages(imagesFromDb):
 def runExperiment(types, collection):
 	experimentResults = {}
 	for t in types:	
-		results = collection.find({"type":t})
+		results = collection.find({"type":t}).sort("date").limit(100)
 		cvImages = convertToImages(results)
 		algorithms = []
 
-		algorithms.append(Algorithm("SIFT" + "_" + t, cv.xfeatures2d.SIFT_create()))
-		algorithms.append(Algorithm("SURF" + "_" + t, cv.xfeatures2d.SURF_create()))
-		algorithms.append(Algorithm("ORB" + "_" + t, cv.ORB_create()))
+		algorithms.append(Algorithm("SIFT", cv.xfeatures2d.SIFT_create()))
+		algorithms.append(Algorithm("SURF", cv.xfeatures2d.SURF_create()))
+		algorithms.append(Algorithm("ORB", cv.ORB_create()))
+		algorithms.append(Algorithm("NEGRI", NegriDetector(80, 255)))
 
 		experimentResults.update(runSerieExtraction(cvImages, algorithms))
-	return experimentResults
+
+	return plotDataFrame(experimentResults)
+
+def plotDataFrame(data):
+	ts = pd.DataFrame.from_dict(data)
+	size = ts[ts.columns[0]].size
+	ts.set_index(pd.date_range('1/1/2011 00:00:00', periods=size, freq='45min')).plot()
+	plt.show()
+
+	return ts
 
 def main():
-	imagesCollection = getCollection('192.168.0.16', 27017, "nephos-test", "images")
+	imagesCollection = getCollection('192.168.0.16', 27017, "nephos", "images")
 	
-	types = imagesCollection.find({}).distinct("type")
+	# types = imagesCollection.find({}).distinct("type")
+	# types = imagesCollection.find({"type": "vis"}).sort("date").limit(3)
+	types = ['ir2']
 	return runExperiment(types, imagesCollection)
 
-experimentResults = main()
+ts = main()
